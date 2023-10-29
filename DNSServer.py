9e74da3ef9418f867e3b762fe7634584d1bb531d
Raspraz3 <147226223+Raspraz3@users.noreply.github.com>
2023-10-28 23:48:12 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -51,6 +51,25 @@ decrypted_value = decrypt_with_aes(encrypted_value, password, salt)
 
 # A dictionary containing DNS records mapping hostnames to different types of DNS data.
 dns_records = {
+
+'example.com.': {
+        dns.rdatatype.A: '192.168.1.101',
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
+        dns.rdatatype.MX: [(10, 'mail.example.com.')],  # List of (preference, mail server) tuples
+        dns.rdatatype.CNAME: 'www.example.com.',
+        dns.rdatatype.NS: 'ns.example.com.',
+        dns.rdatatype.TXT: ('This is a TXT record',),
+        dns.rdatatype.SOA: (
+            'ns1.example.com.', #mname
+            'admin.example.com.', #rname
+            2023081401, #serial
+            3600, #refresh
+            1800, #retry
+            604800, #expire
+            86400, #minimum
+        ),
+    },
+
     'yahoo.com.': {
         dns.rdatatype.A: '192.168.1.105',
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
@@ -114,7 +133,7 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],  # List of (preference, mail server) tuples
         dns.rdatatype.CNAME: 'www.nyu.edu.',
-        dns.rdatatype.NS: 'ns.nyu.edu.',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
         dns.rdatatype.TXT: ('AlwaysWatching',),
         dns.rdatatype.MX: [(10, 'mail.nyu.edu.')],
         dns.rdatatype.SOA: (
