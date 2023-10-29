6f0fd21c3e3e4977f37bb1d8b3c1d303ccf48503
Raspraz3 <147226223+Raspraz3@users.noreply.github.com>
2023-10-29 00:05:38 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -43,7 +43,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = b'Tandon'  # Use bytes for salt
-password = b'dlr391@nyu.edu'
+password = 'dlr391@nyu.edu'
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt)
